cb00b62aa0cfcffa22774bc1cf7477cc1e674b81
elisoleymani <elhamsoleymani019@gmail.com>
2023-11-14 09:31:41 +0100

    RockPaperScissors and JavaRolls are added

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class RockPaperScissors {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) 
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Let's play Rock Paper Scissors.");
